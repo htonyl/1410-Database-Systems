fb01965f7f026f3eb24fee857923ac026f32f8ae
Jonathan Sim <Jonathan Sim>
2017-03-20 23:23:36 +0800

    create patient and room fake data

---
+++ patient_data_script.sql
@@ -0,0 +1,3 @@
+INSERT Patient VALUES 
+('001','Sarah Wu','Female','05/04/1992'),('002','Joe Rosberg','Male','15/07/1999'),('003','John Woodgate','Male','21/03/1979'),
+('004','Alina Ott','Female','28/011/1985'),('005','Jasmine Gomez','Female','15/06/1952'),('006','Toby Verthongen','Male','25/04/1967');
