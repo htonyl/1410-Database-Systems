442fb6e5b87e6761b2ee71e55f66beb4017880e3
tony <duma0311@gmail.com>
2017-04-05 01:55:22 +0200

    Complete table modifications

--- patient_data_script.sql
+++ patient_data_script.sql
@@ -1,7 +1,7 @@
-INSERT Patient VALUES 
-('001','Sarah Wu','Female',STR_TO_DATE('05/04/1992', '%d/%m/%Y')),
-('002','Joe Rosberg','Male',STR_TO_DATE('15/07/1999', '%d/%m/%Y')),
-('003','John Woodgate','Male',STR_TO_DATE('21/03/1979', '%d/%m/%Y')),
-('004','Alina Ott','Female',STR_TO_DATE('28/11/1985', '%d/%m/%Y')),
-('005','Jasmine Gomez','Female',STR_TO_DATE('15/06/1952', '%d/%m/%Y')),
-('006','Toby Verthongen','Male',STR_TO_DATE('25/04/1967', '%d/%m/%Y'));
+INSERT Patient VALUES
+('P001','Sarah Wu','Female',STR_TO_DATE('05/04/1992', '%d/%m/%Y')),
+('P002','Joe Rosberg','Male',STR_TO_DATE('15/07/1999', '%d/%m/%Y')),
+('P003','John Woodgate','Male',STR_TO_DATE('21/03/1979', '%d/%m/%Y')),
+('P004','Alina Ott','Female',STR_TO_DATE('28/11/1985', '%d/%m/%Y')),
+('P005','Jasmine Gomez','Female',STR_TO_DATE('15/06/1952', '%d/%m/%Y')),
+('P006','Toby Verthongen','Male',STR_TO_DATE('25/04/1967', '%d/%m/%Y'));
--- table_modifications.sql
+++ table_modifications.sql
@@ -1,28 +1,31 @@
 USE Hospital;
+SET SQL_SAFE_UPDATES=0;
 
 -- Adding a new row:
-INSERT Patient VALUES ('P008','Steven Eriksen','Male','18/03/1992');
+INSERT Patient VALUES ('P007','Steven Eriksen','Male',STR_TO_DATE('18/03/1992', '%d/%m/%Y'));
 
 -- Alternatively:
 INSERT Patient (PatientID, Name, Gender, Birth)
-VALUES ('P008','Steven Eriksen','Male','18/03/1992');
+VALUES ('P007','Steven Eriksen','Male',STR_TO_DATE('18/03/1992', '%d/%m/%Y'));
 
 -- Adding multiple rows:
 INSERT Patient VALUES
-('008','Steven Eriksen','Male','18/03/1992'),
-('009','Jenna Svensson','Female','04/07/1984'),
-('010','Viktor Christiansen','Male','11/08/1975');
+('P008','John Liu','Male',STR_TO_DATE('11/03/1990', '%d/%m/%Y')),
+('P009','Jenna Svensson','Female',STR_TO_DATE('04/07/1984', '%d/%m/%Y')),
+('P010','Viktor Christiansen','Male',STR_TO_DATE('11/08/1975', '%d/%m/%Y'));
 
 -- Adding with NULL Values:
 
 -- Update multiple values in a row:
-UPDATE Patient SET PatientID = ,Name = ‘’, Gender = Male, Birth = ;
+UPDATE Patient SET Birth = STR_TO_DATE('04/07/1977', '%d/%m/%Y')) WHERE PatientID = 'P009';
 
 -- Update multiple rows:
-
+UPDATE Patient SET Gender = CASE WHEN Gender = 'Male' THEN 'M' ELSE 'F' END;
 
 -- Deleting a row:
-INSERT from Patient where PatientID = P008;
+DELETE FROM Patient WHERE PatientID='P008';
 
 -- Deleting multiple rows:
-INSERT from Patient where Gender = Male;
+DELETE FROM Patient WHERE Gender='Male';
+
+SET SQL_SAFE_UPDATES=1;
