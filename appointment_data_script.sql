086c2e20a1b73943fa876ef6a0caf607c7415d97
pbreau3 <pierre-william.breau@mail.mcgill.ca>
2017-04-05 22:03:57 +0200

    added hosp. and appointment data

---
+++ appointment_data_script.sql
@@ -0,0 +1,7 @@
+INSERT Appointment VALUES
+('A001',STR_TO_DATE('05/04/2007', '%d/%m/%Y'),'d124567','Lauren Holmes','P005','Jasmine Gomez'),
+('A002',STR_TO_DATE('01/06/2013', '%d/%m/%Y'),'d162198','Andrew Lowe','P001','Sarah Wu'),
+('A003',STR_TO_DATE('01/03/2011', '%d/%m/%Y'),'d143267','Carrie Miller','P001','Sarah Wu'),
+('A004',STR_TO_DATE('11/04/2004', '%d/%m/%Y'),'d124567','Lauren Holmes','P006','Toby Verthongen'),
+('A005',STR_TO_DATE('02/02/2002', '%d/%m/%Y'),'d143267','Carrie Miller','P004','Alina Ott'),
+('A006',STR_TO_DATE('09/10/2000', '%d/%m/%Y'),'d162198','Andrew Lowe','P003','John Woodgate');
